c9b0f325ab5521aa0038382d60375c5d8b4a6a26
Nick B <35877456+ambeloe@users.noreply.github.com>
2020-07-03 13:08:29 -0400

    Update mcget.py
    added some extra functions for convenience

--- mcget.py
+++ mcget.py
@@ -1,5 +1,4 @@
 import sys
-import json
 import requests
 
 mcLauncherUrl = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
@@ -15,15 +14,19 @@ def get_latest_version():
     return json["latest"]["release"]
 
 
-def get_jar_url(version):
+def get_meta_url(ver):
     cringe_url = ""
     for i in range(len(json["versions"])):
-        if json["versions"][i]["id"] == version:
+        if json["versions"][i]["id"] == ver:
             cringe_url = json["versions"][i]["url"]
             break
     if cringe_url == "":
         raise Exception("no such version found")
-    version_meta = requests.get(cringe_url).json()
+    return cringe_url
+
+
+def get_jar_url(version):
+    version_meta = requests.get(get_meta_url(version)).json()
     if "server" in version_meta["downloads"]:
         return version_meta["downloads"]["server"]["url"]
     else:
@@ -32,6 +35,31 @@ def get_jar_url(version):
 
 def get_latest_jar():
     return get_jar_url(get_latest_version())
+
+
+def all_versions():
+    vers = []
+    for version in json["versions"]:
+        vers.append(version["id"])
+    return vers
+
+
+def get_channel_versions(chan):
+    vers = []
+    for version in json["versions"]:
+        if version["type"] == chan:
+            vers.append(version["id"])
+    return vers
+
+
+def get_jar_urls(chan):
+    pog = {}
+    for version in get_channel_versions(chan):
+        try:
+            pog.update({version: get_jar_url(version)})
+        except:
+            pog.update({version: ""})
+    return pog
 
 
 if __name__ == "__main__":
